3cc2396aeb26391ff15ec98b3cbcd86810819a2f
nikmorror37 <nikitamironchuk37@gmail.com>
2023-03-07 22:13:25 +0300

    Initial commit

---
+++ HT10TryCatch/HT10TryCatch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HT10TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
